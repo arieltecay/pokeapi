a32479526b7a909b47129432136b3b3ba447f7ce
arieltecay <arieltecay@gmail.com>
2021-03-01 22:05:18 -0300

    Mostrar los datos en la pagina sin diseño

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import NavBar from "./components/Navbar/NavBar";
 import Pokedex from "./components/Pokedex/Pokedex";
 import SearchBar from "./components/SearchBar/SearchBar";
-
+import { getPokemons } from "./api";
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  const fetchPokemons = async () => {
+    try {
+      const data = await getPokemons();
+      setPokemon(data.results);
+      // console.log(data);
+    } catch (error) {}
+  };
+  // console.log("Los Pokemones Son:", pokemon);
+
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
   return (
     <div>
       <NavBar />
       <div>
         <SearchBar />
-        <Pokedex />
+        <Pokedex pokemon={pokemon} />
       </div>
     </div>
   );
--- src/api.js
+++ src/api.js
@@ -8,3 +8,13 @@ export const searchPokemon = async (pok) => {
     console.log("No existe:", error);
   }
 };
+export const getPokemons = async (limit = 5, offset = 0) => {
+  try {
+    let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
